733b7a7359830cb2d0b42ed4f7f548d376ba32c5
Ling Qing Meng <ling.q.meng@gmail.com>
2018-07-30 13:42:36 +0900

    truffle with migrations incomplete

---
+++ migrations/2_deploy_zkp.js
@@ -0,0 +1,9 @@
+const Ownable = artifacts.require("./Ownable.sol");
+const pairing = artifacts.require("./pairing.sol");
+const SafeMath = artifacts.require("./SafeMath.sol");
+const votelottery = artifacts.require("./votelottery.sol");
+
+module.exports = function(deployer) {
+  /** todo: Ling to add in constructor args */
+
+};
